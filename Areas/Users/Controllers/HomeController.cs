44d1533e3b1a357417a45f4ed8759615d0a412a2
duy nguyen <n.khanhduy.230899@gmail.com>
2020-07-13 14:42:06 +0700

    Add LineChart

---
+++ Areas/Users/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebGSMT.Models;
+
+namespace WebGSMT.Areas.Users.Controllers
+{
+    public class HomeController : Controller
+    {
+        GiamSatMoiTruongDbContext _db;
+        public HomeController(GiamSatMoiTruongDbContext db)
+        {
+            _db = db;
+        }
+
+        [Area("Users")]
+        public async Task<IActionResult> Index()
+        {
+            var data = await _db.Datas.ToListAsync();
+            ViewBag.TagName = await _db.Catalog_Datas.ToListAsync();
+            return View(data);
+        }
+    }
+}
