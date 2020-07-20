f43c2fffb65fbb7dd309e76ce9b3c0549e8eb9ea
duy nguyen <n.khanhduy.230899@gmail.com>
2020-07-20 16:48:49 +0700

    New web Device

---
+++ Areas/Users/Controllers/DevicesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebGSMT.Areas.Users
+{
+    [Area("Users")]
+    [Route("{area}/Devices")]
+    public class DevicesController : Controller
+    {
+        [Route("ListDevice")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
