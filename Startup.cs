478c6774bffa17c5c704ff179917b88b054cbafb
duy nguyen <n.khanhduy.230899@gmail.com>
2020-07-30 09:55:32 +0700

    zxc

--- Startup.cs
+++ Startup.cs
@@ -45,12 +45,11 @@ namespace WebGSMT
             services.AddRazorPages().AddRazorRuntimeCompilation();
             services.AddSession();
             services.AddScoped<AuthorizeActionFilter>();
-<<<<<<< HEAD
             services.AddMvc(options => options.EnableEndpointRouting = false);
-=======
+
             services.AddSession();
             services.AddScoped<AuthorizeActionFilter>();
->>>>>>> Authorize/AtributeCustom
+
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
