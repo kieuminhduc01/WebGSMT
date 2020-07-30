d39e91df8ecf1ef7934a259a5b5c0ca6bad486a7
duy nguyen <n.khanhduy.230899@gmail.com>
2020-07-30 11:54:29 +0700

    1234

---
+++ wwwroot/Users_JS/Validate_Users.js
@@ -0,0 +1,12 @@
+﻿function ValidateEmail(inputText) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (inputText.value.match(mailformat)) {
+        document.informationForm.email.focus();
+        return true;
+    }
+    else {
+        alert("You have entered an invalid email address!");
+        document.form1.text1.focus();
+        return false;
+    }
+}
